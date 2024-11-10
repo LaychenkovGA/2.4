a7dc651371e345db2cb5f89133e8d8e35af044b2
Gleb Laychenkov <racers2no95@mail.ru>
2024-11-10 22:45:11 +0400

    Initial commit 3

--- src/test/java/ru/netology/qa/UIAutomatorTest.java
+++ src/test/java/ru/netology/qa/UIAutomatorTest.java
@@ -1,37 +1,45 @@
 package ru.netology.qa;
 
-import io.appium.java_client.android.AndroidDriver;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import ru.netology.qa.screens.MainScreenUIAutomator;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import io.appium.java_client.android.AndroidDriver;
+import ru.netology.qa.screens.MainScreenUIAutomator;
 
-public class homework {
+public class UIAutomatorTest {
 
     private AndroidDriver driver;
 
+    private URL getUrl() {
+        try {
+            return new URL("http://127.0.0.1:4723");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
     @BeforeEach
-    public void setUp() throws MalformedURLException {
+    public void setUp() {
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-        desiredCapabilities.setCapability("platformName", "Android");
-        desiredCapabilities.setCapability("appium:deviceName", "Some name");
+        desiredCapabilities.setCapability("platformName", "android");
+        desiredCapabilities.setCapability("appium:deviceName", "meduimPhone");
         desiredCapabilities.setCapability("appium:appPackage", "ru.netology.testing.uiautomator");
         desiredCapabilities.setCapability("appium:appActivity", "ru.netology.testing.uiautomator.MainActivity");
+        desiredCapabilities.setCapability("appium:automationName", "uiautomator2");
         desiredCapabilities.setCapability("appium:ensureWebviewsHavePages", true);
         desiredCapabilities.setCapability("appium:nativeWebScreenshot", true);
         desiredCapabilities.setCapability("appium:newCommandTimeout", 3600);
         desiredCapabilities.setCapability("appium:connectHardwareKeyboard", true);
 
-        URL remoteUrl = new URL("http://127.0.0.1:4723/wd/hub");
 
-        driver = new AndroidDriver(remoteUrl, desiredCapabilities);
-
+        driver = new AndroidDriver(getUrl(), desiredCapabilities);
     }
 
     @Test
@@ -45,7 +53,7 @@ public class homework {
         main.buttonChange.isDisplayed();
         main.buttonChange.click();
         String text2 = main.textToBeChanged.getText();
-        Assertions.assertEquals(text1,text2);
+        Assertions.assertEquals(text1, text2);
 
     }
 
@@ -55,12 +63,12 @@ public class homework {
 
         main.userInput.isDisplayed();
         main.userInput.click();
-        String newText = "New text";
+        String newText = "TuToK";
         main.userInput.sendKeys((newText));
         main.buttonActivity.isDisplayed();
         main.buttonActivity.click();
         main.activityText.isDisplayed();
-        Assertions.assertEquals(main.activityText.getText(),newText);
+        Assertions.assertEquals(main.activityText.getText(), newText);
 
     }
 
